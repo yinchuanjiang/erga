c1a1119a0b90daaaf606097092a330036622828e
安小游 <471607346@qq.com>
2019-07-15 12:32:51 +0800

    commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,15 +11,11 @@ class AuthController extends Controller
     //静默授权
     public function auth()
     {
-        $appid = config('app.wx_appid');
-        $url = route('home');
-        $scope = 'snsapi_base';
-        $state = '';
-        return redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$url&response_type=code&scope=$scope&state=$state#wechat_redirect");
-    }
-    //非静默授权
-    public function userAuth()
-    {
+//        $appid = config('app.wx_appid');
+//        $url = route('home');
+//        $scope = 'snsapi_base';
+//        $state = '';
+//        return redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$url&response_type=code&scope=$scope&state=$state#wechat_redirect");
         $appid = config('app.wx_appid');
         $url = route('auth.user');
         $scope = 'snsapi_userinfo';
