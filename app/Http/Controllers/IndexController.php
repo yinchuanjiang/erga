de15cf8ec77c0082a0103540a38e1122a9ca002c
安小游 <471607346@qq.com>
2019-07-17 12:32:04 +0800

    commit

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\SubmitRequest;
 use App\UserLuck;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 
 class IndexController extends Controller
 {
