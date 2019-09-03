d599d29d0e319475032aece189bbb606a04e4286
安小游 <471607346@qq.com>
2019-09-03 15:22:42 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+//自定义全局函数返回接口数据
+if (! function_exists('show')) {
+    /**
+     * @param $status
+     * @param string $msg
+     * @param array $data
+     * @return array
+     */
+    function show($status = 200, $msg = '',$data = [])
+    {
+        return new \Illuminate\Http\JsonResponse(compact('status','msg','data'));
+    }
+}
+//Route::get('/auth', 'AuthController@auth')->name('login');
+//Route::get('/home', 'AuthController@authUser')->name('login');
+Route::get('/', 'IndexController@activity')->name('login');
+Route::group(['middleware' => 'auth:web'], function () {
+    Route::get('/luck', 'IndexController@luck')->name('luck');
+    Route::get('/prize', 'IndexController@prize')->name('prize');
+    Route::post('/submit', 'IndexController@submit')->name('submit');
+});
+Route::get('/rule', function () {
+    return view('index.rule');
 });
